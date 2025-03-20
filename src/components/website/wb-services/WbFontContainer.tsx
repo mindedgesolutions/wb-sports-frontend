4f1ddc3ea5334d6fc3edb0bed80ea113eb4d85e3
Souvik Nag <s.nag26@gmail.com>
2025-03-20 22:16:23 +0530

    news

--- src/components/website/wb-services/WbFontContainer.tsx
+++ src/components/website/wb-services/WbFontContainer.tsx
@@ -1,41 +1,49 @@
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
-import { ALargeSmall } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import { ALargeSmall, Minus, Plus } from 'lucide-react';
 import { useState } from 'react';
 
 const WbFontContainer = () => {
-  const [fontsize, setFontsize] = useState('text-sm');
+  const [pulsingPlus, setPulsingPlus] = useState(false);
+  const [pulsingMinus, setPulsingMinus] = useState(false);
+
+  const increment = () => {
+    setPulsingPlus(true);
+    setTimeout(() => setPulsingPlus(false), 100);
+  };
+
+  const decrement = () => {
+    setPulsingMinus(true);
+    setTimeout(() => setPulsingMinus(false), 100);
+  };
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger className="data-[state=closed]:border-none data-[state=closed]:outline-none cursor-pointer">
-        <ALargeSmall size={24} color="white" />
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="bg-sky-900 border-none">
-        <DropdownMenuItem
-          className="text-sm font-normal text-primary-foreground cursor-pointer focus:bg-sky-700 focus:text-white font-roboto"
-          // onClick={() => setFontsize()}
-        >
-          Abc
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          className="text-xl font-normal text-primary-foreground cursor-pointer focus:bg-sky-700 focus:text-white font-roboto"
-          // onClick={() => setFontsize()}
-        >
-          Abc
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          className="text-3xl font-normal text-primary-foreground cursor-pointer focus:bg-sky-700 focus:text-white font-roboto"
-          // onClick={() => setFontsize()}
-        >
-          Abc
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <div className="flex flex-row justify-center items-center bg-sky-800 rounded-md">
+      <Button
+        className="relative bg-transparent hover:bg-transparent"
+        onClick={increment}
+      >
+        <Plus size={24} color="white" />
+        {pulsingPlus && (
+          <span className="absolute inset-0 flex items-center justify-center">
+            <span className="w-full h-full bg-blue-500 opacity-50 rounded-full animate-ping"></span>
+          </span>
+        )}
+      </Button>
+      <section className="flex flex-row gap-2 justify-center items-center text-white">
+        <ALargeSmall />
+      </section>
+      <Button
+        className="relative bg-transparent hover:bg-transparent"
+        onClick={decrement}
+      >
+        <Minus size={24} color="white" />
+        {pulsingMinus && (
+          <span className="absolute inset-0 flex items-center justify-center">
+            <span className="w-full h-full bg-blue-500 opacity-50 rounded-full animate-ping"></span>
+          </span>
+        )}
+      </Button>
+    </div>
   );
 };
 export default WbFontContainer;
---
+++ src/components/website/wb-services/WbMenu.tsx
@@ -0,0 +1,8 @@
+const WbMenu = () => {
+  return (
+    <div className="bg-sky-600">
+      <div className="max-w-screen-xl mx-auto">Menu</div>
+    </div>
+  );
+};
+export default WbMenu;
